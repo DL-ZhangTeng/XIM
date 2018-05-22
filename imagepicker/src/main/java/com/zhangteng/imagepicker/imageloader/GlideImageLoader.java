f3ab364f8bf220288eb3530f6913946ecf955175
duoluo9 <763263368@qq.com>
2018-05-22 17:05:58 +0800

    添加图片选择器

---
+++ imagepicker/src/main/java/com/zhangteng/imagepicker/imageloader/GlideImageLoader.java
@@ -0,0 +1,24 @@
+package com.zhangteng.imagepicker.imageloader;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.zhangteng.imagepicker.R;
+
+/**
+ * Created by swing on 2018/4/18.
+ */
+public class GlideImageLoader implements ImageLoader {
+    @Override
+    public void loadImage(Context context, ImageView imageView, String uri) {
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.mipmap.image_picker_photo)
+                .centerCrop();
+        Glide.with(context)
+                .load(uri)
+                .apply(requestOptions)
+                .into(imageView);
+    }
+}
