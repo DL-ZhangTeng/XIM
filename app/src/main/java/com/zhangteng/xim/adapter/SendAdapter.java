5440c0c2df9954fb654c001b0232f499d873cd64
duoluo9 <763263368@qq.com>
2018-06-01 17:39:31 +0800

    添加聊天页每行最多13个中文

--- app/src/main/java/com/zhangteng/xim/adapter/SendAdapter.java
+++ app/src/main/java/com/zhangteng/xim/adapter/SendAdapter.java
@@ -6,7 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -80,7 +80,7 @@ public class SendAdapter extends BaseAdapter<BmobIMMessage> {
 
     public class MessageViewHolder extends RecyclerView.ViewHolder {
         private CircleImageView circleImageView;
-        private Button message;
+        private TextView message;
 
         public MessageViewHolder(View itemView) {
             super(itemView);
